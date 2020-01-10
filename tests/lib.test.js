69b3de997d0e18b4da1f96b5ee721dca51c053ec
DrGooseman <jamesjquinn12@gmail.com>
2020-01-10 15:35:37 +0300

    more tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -18,3 +18,52 @@ describe("absolute", () => {
     });
 
 });
+
+describe("greet", () => {
+
+    test ("should return the greeting message", () => {
+        const result = lib.greet("Mosh");
+        expect(result).toMatch(/Mosh/);
+    });
+  
+});
+
+describe("getCurrencies", () => {
+
+    test ("should return supported currencies", () => {
+        const result = lib.getCurrencies();
+
+       // expect(result).toContain("USD");
+        //expect(result).toContain("AUD");
+       // expect(result).toContain("EUR");
+
+        expect(result).toEqual(expect.arrayContaining(["USD", "AUD", "EUR"]));
+    });
+  
+});
+
+describe("getProduct", () => {
+
+    test ("should return the product with the given id", () => {
+        const result = lib.getProduct(1);
+
+        expect(result).toEqual({ id:1, price: 10 });
+      
+    });
+  
+});
+
+describe("registerUser", () => {
+    test ("should throw if username is falsy", () => {
+        const args = [null, undefined, NaN, "", 0, false];
+        args.forEach(a => {
+            expect(() => lib.registerUser(a)).toThrow();
+        })
+    });
+
+    test ("should return a user object if valid username is passed", () => {
+        const result = lib.registerUser("mosh");
+        expect(result).toMatchObject({ username: "mosh" });
+        expect(result.id).toBeGreaterThan(0);
+    });
+});
