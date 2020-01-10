df055aa4cbc796c0ed5f6508819668cda14e226a
DrGooseman <jamesjquinn12@gmail.com>
2020-01-10 11:32:45 +0300

    first tests, absolute function

---
+++ tests/lib.test.js
@@ -0,0 +1,20 @@
+const lib = require("../lib");
+
+describe("absolute", () => {
+
+    test ("should return positive number if input is positive", () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    test ("should return positive number if input is negative", () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    test ("should return 0 number if input is 0", () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+
+});
